10f979abc7207b5dcad42aa27e56dde22a4f1015
Saighna <57232342+Saighna@users.noreply.github.com>
2022-11-29 23:53:18 +0530

    Add files via upload

---
+++ post.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit{
+
+  postForm!:FormGroup;
+  editForm!:FormGroup;
+  postDetails:any;
+  postData:any=[]
+  postObj: Post={
+    id: '',
+    title: '',
+    post: ''
+  }
+  constructor(private fb:FormBuilder,private postService:PostService){
+    this.postForm= this.fb.group({
+      title:['',Validators.required],
+      post:['',Validators.required],
+    });
+    this.editForm= this.fb.group({
+      edit_title:['',Validators.required],
+      edit_post:['',Validators.required],
+    })
+  }
+
+
+  ngOnInit(){
+    this.getAllPosts()
+  }
+
+  addPost(){
+    const{value}=this.postForm
+    console.log(value)
+    this.postObj.id='',
+    this.postObj.title=value.title,
+    this.postObj.post=value.post
+
+    this.postService.addPost(this.postObj).then((post)=>{
+      if(post){
+        alert("Posted sucessfully")
+        this.postForm.reset();
+      }
+    })
+  }
+
+  getAllPosts(){
+    this.postService.getPosts().subscribe((res:Post[])=>{
+      console.log(res)
+      this.postData=res;
+    })
+  }
+
+  deletePost(post:Post){
+    let c=confirm("Are you sure, do you want to delete?");
+    if(c==true){
+      this.postService.deletePost(post);
+    }
+  }
+  getAllDetails(post:Post){
+    this.postDetails=post
+    console.log(this.postDetails)
+  }
+  updatePost(post:Post){
+    const{value}=this.editForm
+    console.log(value);
+    (this.postObj.id=post.id),
+    (this.postObj.title=value.edit_title),
+    (this.postObj.post=value.edit_post);
+    this.postService.updatePost(post, this.postObj).then(()=>{
+      alert("Updated Sucessfully")
+    })
+    this.editForm.reset()
+  }
+}
